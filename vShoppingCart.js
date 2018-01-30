acd9ea399a1feba6b11cdd05526f2fc79fef1a81
Gerald Aburto <gerald.aburto@nestrixcorp.com>
2018-01-30 12:32:57 -0600

    Shopping Cart functionality

--- vShoppingCart.js
+++ vShoppingCart.js
@@ -1,11 +1,19 @@
-(function (document, window) {
-    'use strict';
+/* 
+*   Vanilla JavaScript Shopping Cart (vShoppingCart) v0.1.0 
+*   Author: Gerald Aburto <https://github.com/Gerald-AuZ>
+*   Repository: https://github.com/Gerald-AuZ/vShoppingCart
+*   License: MIT
+*/
+(function (document, window) { 'use strict';
 
-    const _this = {
+    let _this = {
         shoppingCart: JSON.parse(localStorage.getItem('vShoppingCart')) || {
             count: 0,
             total: 0,
             items: []
+        },
+        updateCart: function () {
+            localStorage.setItem("vShoppingCart", JSON.stringify(this.shoppingCart));
         }
     };
 
@@ -24,26 +32,47 @@
                 throw 'Item must have a name.';
             if (!item.price)
                 throw 'Item must have a price.';
-            if (!item.count)
+            if (!item.quantity)
                 throw 'Item must have a count';
+            if (!item.image)
+                throw 'Item must have an image';
 
             var _item = {
                 id: item.id,
                 name: item.name,
                 price: item.price,
                 discount: item.discount || 0.0,
-                count: item.count,
+                quantity: item.quantity,
+                image: item.image,
                 plusOne: function () {
-                    this.count++;
+                    this.quantity++;
+                    _this.shoppingCart.total += (this.price - this.discount);
+                    _this.updateCart();
                 },
                 subtractOne: function () {
-                    if (this.count > 1)
-                        this.count--;
+                    if (this.quantity == 1) return;
+                    this.quantity--;
+                    _this.shoppingCart.total -= (this.price - this.discount);
+                    _this.updateCart();
+                },
+                removeFromCart: function () {
+                    let index = _this.shoppingCart.items.indexOf(this);
+                    _this.shoppingCart.items.splice(index, 1);
+                    _this.updateCart();
                 }
             };
             _this.shoppingCart.items.push(_item);
             _this.shoppingCart.count++;
-            localStorage.setItem("vShoppingCart", JSON.stringify(_this.shoppingCart));
+            _this.shoppingCart.total += (_this.price - _this.discount);
+            _this.updateCart();
+        },
+        count: _this.shoppingCart.count,
+        clean: function () {
+            localStorage.setItem("vShoppingCart", JSON.stringify({
+                count: 0,
+                total: 0,
+                items: []
+            }));
         }
     };
 
